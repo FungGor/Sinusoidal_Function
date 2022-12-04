0a4f7d554acd22bfabd98935d915bf53435c1d7e
Ki Fung Leung <kifungleung19970611@gmail.com>
2022-12-04 14:47:40 +0800

    Factorial Function has been added !

--- Sinusoidal_Function/main.cpp
+++ Sinusoidal_Function/main.cpp
@@ -19,23 +19,57 @@ double result = 0;
 unsigned int sum = 0;
 unsigned int multiple = 0;
 
+//Compute factorial
+int factorial_calculation(int sum)
+{
+    int result = 0;
+    //For example
+    if (sum == 1)
+        result = 1;
+    else
+    {
+        result = sum;
+        //example result = 3;   --> result = 5
+        while (sum > 0)
+        {
+            //1st iteration : sum = 2
+            //2nd iteration : sum = 1
+            //if the result = 5, then
+            //1st iteration : sum = 4
+            //2nd iteration : sum = 3
+            //3rd iteration : sum = 2
+            //4th iteration : sum = 1
+            sum -- ;
+            if(sum == 0)
+                break;
+            //1st iteration : result = 3 * 2 = 6
+            //2nd iteration : result = 6 * 1 = 6
+            //if the result = 5 :
+            //1st iteration : result = 5 * 4 = 20
+            //2nd iteration : result = 20 * 3 = 60
+            //3rd iteration : result = 60 * 2 = 120
+            //4th iteration : result = 120 * 1 = 120
+            result = result * sum;
+        }
+    }
+    return result;
+}
+
+//Compute the sinusoidal function in taylor series approach
 int main() {
 
     // insert code here...
     cout << "Hello, World!\n";
     cout << "Please input the number:"<<endl;
     cin >> number;
+    cout << endl;
     
     //Get starting timepoint
     auto start = high_resolution_clock::now();
     for (int i = 0; i<=number; i++)
     {
         sum = (2*i)+1;
-        if (multiple == 0)
-            multiple = 1;
-        multiple = sum * multiple;
-        multiple ++;
-        
+        cout << "The first group is: "<<sum <<"   "<<"Factorial :"<<factorial_calculation(sum)<<endl;
     }
     
     //Get enging timepoint
@@ -48,5 +82,6 @@ int main() {
     
     cout<<"Time taken by for loop "<<duration.count()<<" microseconds"<<endl;
     
+    cout<<"the end"<<endl;
     return 0;
 }
